0d6ed3b516896681c5f504acf6f05021f5ae1818
gruelt <gruelt@gmail.com>
2021-11-10 00:17:49 +0100

    gitignore

--- pages/elements/index.js
+++ pages/elements/index.js
@@ -3,9 +3,10 @@ import Layout from "../../components/layout/layout";
 import utilStyles from "../../styles/utils.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import {Grid} from "@mui/material";
 
 export async function getStaticProps(context) {
-    const res = await fetch(`http://localhost:8000/api/elements`)
+    const res = await fetch(process.env.API_URL+'/api/elements');
     const data = await res.json()
     console.log(data)
     if (!data) {
@@ -29,6 +30,32 @@ export default function Elements({ data }) {
             {/* Keep the existing code here */}
 
             {/* Add this <section> tag below the existing <section> tag */}
+
+            <Grid container spacing={1}>
+
+            {data.map((elements) => (
+
+                    <Grid item xs={2}>
+
+
+                        <Image
+                            width={250}
+                            height={150}
+                            src={'https://ufolepbrochure.s3.eu-west-3.amazonaws.com/' + elements.image}
+                        ></Image>
+
+                        {/*{elements.nom}*/}
+
+                    </Grid>
+
+
+            ))}
+            </Grid>
+
+
+
+
+
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
 
                 <ul className={utilStyles.list}>
@@ -37,12 +64,13 @@ export default function Elements({ data }) {
                     {data.map((elements) => (
                         <li class="" className={utilStyles.listItem} >
                             <b>{elements.id}</b>
+                            {elements.difficulte}
                             <br/>
                             {elements.nom}
                             <br/>
-                            <i>{elements.image}</i>
+
                             <Image
-                                width={200}
+                                width={250}
                                 height={150}
                                 src={'https://ufolepbrochure.s3.eu-west-3.amazonaws.com/' + elements.image}
                             ></Image>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Layout from "../components/layout/layout";
 
 export default function Home({data}) {
+
+    const api = process.env.API_URL;
+    console.log(api);
+
+
   return (
       <Layout>
 
-        contenu
+        contenus {api}
 
       </Layout>
   )
